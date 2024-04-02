43ea3fc23e7b332d0fab86646573d9509e55165e
coryjacoblewis <109899446+coryjacoblewis@users.noreply.github.com>
2024-04-01 20:06:47 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import art
+
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+def check_answer(guess, answer, turns):
+    if guess > answer:
+        print("Too high.")
+        return turns - 1
+    elif guess < answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {answer} ")
+
+def set_difficulty():
+    level = input("Choose a level. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+def game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = random.randint(1, 100)
+    print(f"pstt. answer is {answer}")
+
+    turns = set_difficulty()
+
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining.")
+        guess = int(input("Make a guess between 1 and 100: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses, you lose")
+            return
+
+print(art.logo)
+
+game()
